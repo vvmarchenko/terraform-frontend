99e2d2993687441e22edb5575525a6dd86042f46
Vladyslav Marchenko <111642735+vvmarchenko@users.noreply.github.com>
2023-02-19 23:07:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,7 @@ module "frontend-conduit" {
   source = "./modules"
   bucket_name = "my-bucket-vmarchenko"
   profile_name = "default"
+  owner_tag = "Vladyslav Marchenko"
+  project_tag = "conduit"
 }
 
